a61b33b1176f789ec45413c23b9da5ee40256640
mansurfayziev <mansurfayziev005@gmail.com>
2024-04-05 03:53:33 +0700

    initial commit

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from contextlib import asynccontextmanager
 from database import delete_tables, create_tables
 from router import router as task_router
 
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await delete_tables()
